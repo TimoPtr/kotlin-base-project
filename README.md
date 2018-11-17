13bf329ff54ee37a958ae12366029bcff6cb5af9
timo <timothy.nibeaudeau@etu.univ-tours.fr>
2018-11-17 14:31:25 +0100

    Initialise project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-kotlin-base-project
+# Base project for Kotlin and build script Kotlin DSL
+
+- Gradle version 4.10.2
+- Kotlin version 1.2.61
+- buildSrc to handle all the build logic
---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
